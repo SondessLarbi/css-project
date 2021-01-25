fb30d511f8db829fd8da7e0e12597ab2dc48bbc7
SondessLarbi <sondesslarbi66@gmail.com>
2021-01-25 15:53:30 +0100

    fix style

--- js/app.js
+++ js/app.js
@@ -2,29 +2,60 @@
 
 
 
-const PropertyList = document.querySelector(".PropertyList")
-const myFetch = fetch('https://f7daacb2-ea37-4c20-9719-dc271658afb7.mock.pstmn.io/v3/property')
-myFetch.then(function(response){
-    return response.json();
-}).then(function(obj){
-    console.log(obj);
-}).catch(function(error){
-    console.error('hhhhhhhhhhh');
+let PropertyList = document.querySelector(".propertyList")
+let PropertyValue = document.querySelector(".propertyValue")
+let propList = document.querySelector(".propList")
+
+fetch('https://f7daacb2-ea37-4c20-9719-dc271658afb7.mock.pstmn.io/v7/property')
+.then((response)=> response.json())
+.then((properties)=>{
+    let output = ""
+    let outputProperty = ""
+let  outputList = ""
+
+//     properties.map(proplist => {
+//         (output += `
+//         <div>
+//   <p class="propertyTitle">${proplist.name}</p>
+
+//   </div>
+// </div>
+// `
+// )})
+// propList.innerHTML = outputList
+
+
+    properties.map(Property => {
+                      (output += `
+                      <div>
+                <div class="TitleAndTry"><p class="propertyTitle">${Property.name}</p>
+                <a href='https://jsfiddle.net/8r936ck1/'>try it yourself</a></div>
+                <p >${Property.definition}</p>
+                <img   class="propertyImg"  src=${Property.Img}></img>
+                <div>
+                </div>
+              </div>
+              `
+              )
+                            
+                Property.propertyValue.forEach(value=>{
+                    (outputProperty += `
+                    <div>
+              <div class="propValue">
+              <div><p >${Property.name} : ${value.value}</p>
+              <p >${value.description}</p></div>
+              <div ><img src=${value.propertyImg}></img></div>
+              </div>
+            </div>
+            `)
+                })
+
+              PropertyList.innerHTML = output
+              PropertyValue.innerHTML = outputProperty
+
+    });
+
+    })
+      .catch(function(error){
 console.error(error);
 });
-
-const showProperties = () => {
-  let output = ""
-  myFetch.forEach(
-    ({ name }) =>
-      (output += `
-              <div class="card">
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  )
-  PropertyList.innerHTML = output
-}
-
-document.addEventListener("DOMContentLoaded", showProperties)
