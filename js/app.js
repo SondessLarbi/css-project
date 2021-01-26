aff52594c62bb744f45e6e5dacc0044e8f113d08
SondessLarbi <sondesslarbi66@gmail.com>
2021-01-26 10:51:22 +0100

    add property images

--- js/app.js
+++ js/app.js
@@ -1,97 +1,11 @@
-let data =[
-    {
-          "name":"align-content",
-          "definition": "align-content property aligns the content vertically (along cross/block/column axis) within a container. The container adjusts its padding so as to align the content vertically. We are using a flex container",
-          "Img":"./images/normal.png",
-      
-          "propertyValue": [
-              {
-                "name":"normal",
-                  "description":"It stretches out the descendant elements to fill the height completely.",
-                  "value":"normal",
-                  "propertyImg":"../images/normal.png"
-              },
-              {
-                "name":"normal",
-                  "description":"It stretches out the descendant elements to fill the height completely.",
-                  "value":"normal",
-                  "propertyImg":"https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.comC/O"
-              },
-              {
-                "name":"normal",
-                  "description":"It stretches out the descendant elements to fill the height completely.",
-                  "value":"normal",
-                  "propertyImg":"https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.comC/O"
-              },
-              {
-                "name":"normal",
-                  "description":"It stretches out the descendant elements to fill the height completely.",
-                  "value":"normal",
-                  "propertyImg":"https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.comC/O"
-              },
-              {
-                "name":"normal",
-                  "description":"It stretches out the descendant elements to fill the height completely.",
-                  "value":"normal",
-                  "propertyImg":"https://via.placeholder.com/150/0000FF/808080 ?Text=Digital.comC/O"
-              }
-              ]
-  }
-  ]
-
 
 
 let propertyDefinition = document.querySelector(".propertyDefinition")
 let PropertyValue = document.querySelector(".propertyValue")
 let propList = document.querySelector(".propList")
-//  let output =""
-//  let outputProperty = ""
-
-// var numbers = [65, 44, 12, 4];
-// let newarr = data.map(myFunction)
 
 
-        // data.map(Property => {
-        //     (output += `
-        //         <div>
-        //         <div class="TitleAndTry"><p class="propertyTitle">${Property.name}</p>
-        //         <a href='https://jsfiddle.net/8r936ck1/'>try it yourself</a></div>
-        //         <p >${Property.definition}</p>
-        //         <img src=${Property.Img}></img>
-        //         <div>
-                
-        //         </div>
-        //       </div>
-        //       `
-        //     )
-            // data.propertyValue.map(value => {
-            //     (outputProperty += `
-            //         <div>
-            //   <div class="propValue">
-            //   <div><p >${Property.name} : ${value.value}</p>
-            //   <p >${value.description}</p></div>
-            //   <div ><img src=${value.propertyImg}></img></div>
-            //   </div>
-            // </div>
-            // `)
-            // })
-        // propertyDefinition.innerHTML = output
-        // PropertyValue.innerHTML = outputProperty
-        // })
-
-
-
-
-
-
-// fetch("../data.json")
-// .then(function(res){
-//     return res.json
-// })
-// .then(function(data){
-//     console.log(data)
-// })
-fetch('https://f7daacb2-ea37-4c20-9719-dc271658afb7.mock.pstmn.io/v7/property')
+fetch('../data.json')
     .then((response) => response.json())
     .then((properties) => {
         let output = ""
@@ -99,32 +13,32 @@ fetch('https://f7daacb2-ea37-4c20-9719-dc271658afb7.mock.pstmn.io/v7/property')
         let outputList = ""
 
         properties.map(Property => {
+            Property.propertyValue.map(value => {
+                (outputProperty += `
+                    <div>
+              <div class="propValue">
+              <div><p >${Property.name} : ${value.value}</p>
+              <p class="descreptionProperty">${value.description}</p></div>
+              <div ><img style="width:150px" src=${value.propertyImg}></img></div>
+              </div>
+            </div>
+            `)
+            }),
             (output += `
                       <div>
                 <div class="TitleAndTry"><p class="propertyTitle">${Property.name}</p>
                 <a href='https://jsfiddle.net/8r936ck1/'>try it yourself</a></div>
                 <p >${Property.definition}</p>
                 <div>
+                ${outputProperty}
                 </div>
               </div>
               `
             )
 
-            Property.propertyValue.forEach(value => {
-                (outputProperty += `
-                    <div>
-              <div class="propValue">
-              <div><p >${Property.name} : ${value.value}</p>
-              <p >${value.description}</p></div>
-              <div ><img src=${value.propertyImg}></img></div>
-              </div>
-            </div>
-            `)
-            })
+
 
 propertyDefinition.innerHTML = output
-            PropertyValue.innerHTML = outputProperty
-
         });
 
     })
