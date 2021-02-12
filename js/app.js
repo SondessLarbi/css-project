e1c0ba811a9bd18ded68f7d2576bdd77711df78c
sondess <sondess.larbi@baaz.com>
2021-02-12 16:14:34 +0100

    change layout

--- js/app.js
+++ js/app.js
@@ -1,62 +1,169 @@
 
+data=[
+    {
+          "name":"align-content",
+          "definition": "align-content property aligns the content vertically (along cross/block/column axis) within a container. The container adjusts its padding so as to align the content vertically. We are using a flex container",
+          "Img":"https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png",
+      
+          "propertyValue": [
+              {
+                "name":"flex-start",
+                  "description":"lines packed to the start of the container.",
+                  "value":"flex-start",
+                  "propertyImg":"../images/flex-start.png"
+              },
+              {
+                "name":"flex-end",
+                  "description":"lines packed to the end of the container.",
+                  "value":"flex-end",
+                  "propertyImg":"../images/flex-end.png"
+              },
+              {
+                "name":"center",
+                  "description":"lines packed to the center of the container.",
+                  "value":"center",
+                  "propertyImg":"../images/center.png"
+              },
+              {
+                "name":"space-between",
+                  "description":"lines evenly distributed; the first line is at the start of the container while the last one is at the end.",
+                  "value":"space-between",
+                  "propertyImg":"../images/space-between.png"
+              },
+              {
+                "name":"space-around",
+                  "description":"lines evenly distributed with equal space between them.",
+                  "value":"space-around",
+                  "propertyImg":"../images/space-around.png"
+              }
+              ]
+  },
+  {
+    "name":"align-items",
+    "definition": "align-content property aligns the content vertically (along cross/block/column axis) within a container. The container adjusts its padding so as to align the content vertically. We are using a flex container",
+    "Img":"https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png",
 
-let property = document.querySelector(".property")
+    "propertyValue": [
+        {
+          "name":"flex-start",
+            "description":"lines packed to the start of the container.",
+            "value":"flex-start",
+            "propertyImg":"../images/flex-start.png"
+        },
+        {
+          "name":"flex-end",
+            "description":"lines packed to the end of the container.",
+            "value":"flex-end",
+            "propertyImg":"../images/flex-end.png"
+        },
+        {
+          "name":"center",
+            "description":"lines packed to the center of the container.",
+            "value":"center",
+            "propertyImg":"../images/center.png"
+        },
+        {
+          "name":"space-between",
+            "description":"lines evenly distributed; the first line is at the start of the container while the last one is at the end.",
+            "value":"space-between",
+            "propertyImg":"../images/space-between.png"
+        },
+        {
+          "name":"space-around",
+            "description":"lines evenly distributed with equal space between them.",
+            "value":"space-around",
+            "propertyImg":"../images/space-around.png"
+        }
+        ]
+},
+{
+  "name":"align-self",
+  "definition": "align-content property aligns the content vertically (along cross/block/column axis) within a container. The container adjusts its padding so as to align the content vertically. We are using a flex container",
+  "Img":"https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png",
+
+  "propertyValue": [
+      {
+        "name":"flex-start",
+          "description":"lines packed to the start of the container.",
+          "value":"flex-start",
+          "propertyImg":"../images/flex-start.png"
+      },
+      {
+        "name":"flex-end",
+          "description":"lines packed to the end of the container.",
+          "value":"flex-end",
+          "propertyImg":"../images/flex-end.png"
+      },
+      {
+        "name":"center",
+          "description":"lines packed to the center of the container.",
+          "value":"center",
+          "propertyImg":"../images/center.png"
+      },
+      {
+        "name":"space-between",
+          "description":"lines evenly distributed; the first line is at the start of the container while the last one is at the end.",
+          "value":"space-between",
+          "propertyImg":"../images/space-between.png"
+      },
+      {
+        "name":"space-around",
+          "description":"lines evenly distributed with equal space between them.",
+          "value":"space-around",
+          "propertyImg":"../images/space-around.png"
+      }
+      ]
+}
+  ]
+
+let property = document.querySelector(".container")
 let PropertyValue = document.querySelector(".propertyValue")
 let propertyList = document.querySelector(".propertyList")
 
 
-fetch('../data.json')
-    .then((response) => response.json())
-    .then((properties) => {
+// fetch('../data.json')
+//     .then((response) => response.json())
+//     .then((properties) => {
         let output = ""
         let outputProperty = ""
-        let outputList = ""
-        properties.map(Property => {
-            console.log(Property.name,"hahahhaahhahahhahlist")
-        })
-        properties.map(Property => {
+        data.map(Property => {
             Property.propertyValue.map(value => {
                 (outputProperty += `
-                    <div>
-              <div class="propValue">
-              <div><p >${Property.name} : ${value.value}</p>
-              <p class="descreptionProperty">${value.description}</p></div>
-              <div ><img style="width:150px" src=${value.propertyImg}></img></div>
-              </div>
+
+                <div class="radio">
+                <input autocomplete="off" type="radio">
+                <label>${Property.name}</label>
             </div>
             `)
             }),
             (output += `
+                      <div class="property">
                       <div>
-                <div class="TitleAndTry"><p class="propertyTitle">${Property.name}</p>
-                <a class="tryJsfiddle" href='https://jsfiddle.net/8r936ck1/'>try it yourself</a></div>
-                <div class="definition" >
-                <p class="defCont">Definition</p>
-                <p class="defProp">${Property.definition}</p>
-                </div>
-                <div class="value">
-                <p class="ValueCont">Property values</p>
-                ${outputProperty}
-                </div>
+                      <p>${Property.name}</p>
+                      <p>${Property.definition}</p>
+                  </div>
+                  <div class="karama">
+                  <div class="codeContainer">
+                  <div class="propertyName">${Property.name} property</div>
+                  <form >
+                  ${outputProperty}
+                  </form>
+                  </div>
+                  <div class="resultContainer">
+                  <div class="propertyName">Result :</div>
+                  <div class="resultDiv"></div>
+              </div>
+                  </div>
+                  <div>Click the property values above to see the result</div>
               </div>
               `
             )
-
-
-
             property.innerHTML = output
         });
 
-    })
-    .catch(function (error) {
-        console.error(error);
-    });
 
-    fetch('../data.json')
-    .then((response) => response.json())
-    .then((properties) => {
         let outputList = ""
-        properties.map(Property => {
+        data.map(Property => {
             (outputList += `
             <div class="list">
                     <a class="proplist" href="https://www.w3schools.com">${Property.name}</a>
@@ -66,9 +173,6 @@ fetch('../data.json')
             propertyList.innerHTML = outputList
 
         })
-
-
-    })
     .catch(function (error) {
         console.error(error);
     });
